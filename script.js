72753039b08419ed9dbc035e038b7dd51e68907e
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-02 18:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+  event.preventDefault();
+  const form = document.getElementById("form1");
+  const fname = form.elements["fname"].value.trim();
+  const lname = form.elements["lname"].value.trim();
+  alert(`${fname} ${lname}`); 
 }
